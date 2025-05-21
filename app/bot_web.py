45fff368f214339a7fcb986287f2fba12d30eeee
Anastasia Dorofeeva <78908868+Doranastasia@users.noreply.github.com>
2025-05-21 15:50:14 +0300

    Update bot_web.py

--- app/bot_web.py
+++ app/bot_web.py
@@ -33,7 +33,6 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 # Генераторы
 from generator import generate_design
 from hybrid_model_web import load_hybrid_model, transform, denormalize
-# generate_custom определяем здесь, используя загруженную гибридную модель
 
 transform = transforms.Compose([
     transforms.Resize((256, 256)),
